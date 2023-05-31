72a774e6d968037fc74b869b24b35b46826c3551
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-05-30 21:39:02 -0400

    ProductCard: CSS

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -4,13 +4,13 @@ export default function ProductCard({
   product: { id, new__image, new__name, new__category, new__price },
 }) {
   return (
-    <li>
-      <img src={new__image} alt={new__name} />
-      <div>
-        <h3>{new__name}</h3>
+    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+      <img className='w-full' src={new__image} alt={new__name} />
+      <div className='mt-2 px-2 text-lg flex justify-between items-center'>
+        <h3 className='truncate'>{new__name}</h3>
         <p>{`${new__price}`}</p>
       </div>
-      <p>{new__category}</p>
+      <p className='mb-2 px-2 text-gray-600'>{new__category}</p>
     </li>
   );
 }
