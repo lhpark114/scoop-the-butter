6d423c310940f01f1d42e4d043a5444b9ecfa625
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-06-02 20:05:50 -0400

    ProductDetail

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export default function ProductCard({
+  product,
   product: { id, new__image, new__name, new__category, new__price },
-}) {
+}) 
+  
+{
+  const navigate = useNavigate();
   return (
-    <li className='rounded-lg shadow-md overflow-hidden cursor-pointer'>
+    <li onClick={() =>{navigate(`/products/${id}`, { state: { product } });}}
+    className='rounded-lg shadow-md overflow-hidden cursor-pointer transition-all hover:scale-105'>
       <img className='w-full' src={new__image} alt={new__name} />
       <div className='mt-2 px-2 text-lg flex justify-between items-center'>
         <h3 className='truncate'>{new__name}</h3>
--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,6 +1,61 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useLocation } from 'react-router-dom';
+import Button from '../components/ui/Button';
 
-export default function ProductDetail() {
-  return <>ProductDetail</>;
+export default function ProductDetail(product) {
+  const {
+    state: {
+      product: {
+        id,
+        new__image,
+        new__name,
+        new__category,
+        new__price,
+        new__description,
+        new__size,
+      },
+    },
+  } = useLocation();
+  const [selected, setSelected] = useState(new__size && new__size[0]);
+  const handleSelect = (e) => setSelected(e.target.value);
+  const handleClick = (e) => {
+    console.log('Added Cart');
+  };
+  return (
+    <>
+      <p className='mx-12 mt-4 text-gray-700'>{new__category}</p>
+
+      <section className='flex flex-col md:flex-row p-4'>
+        <img
+          className='w-full px-4 basis-7/12'
+          src={new__image}
+          alt={new__name}></img>
+        <div className='w-full basis-5/12 flex flex-col p-4'>
+          <h2 className='text-3xl font-bold py-2'>{new__name}</h2>
+          <p className='text-2xl font-bold py-2 border-b border-gray-400'>
+            {new__price}
+          </p>
+          <p className='py-4 text-lg'>{new__description}</p>
+          <div className='flex items-center'>
+            <label className='text-brand font-bold' htmlFor='select'>
+              Options:{' '}
+            </label>
+            <select
+              id='select'
+              className='p-2 m-4 flex-1 border-2 border-dashed border-brand outline-none'
+              onChange={handleSelect}
+              value={selected}>
+              {new__size &&
+                new__size.map((size, index) => (
+                  <option key={index}>{size}</option>
+                ))}
+            </select>
+          </div>
+          <Button text='Add Cart' onClick={handleClick}>
+            Add Cart
+          </Button>
+        </div>
+      </section>
+    </>
+  );
 }
