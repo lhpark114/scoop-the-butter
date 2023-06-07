4b4d7ed35796105ba6fc0c8641591482bddabd68
Chocolate covered Strawberry <75491899+lhpark114@users.noreply.github.com>
2023-06-06 21:47:09 -0400

    emailjs2

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,46 +1,48 @@
-import React, { useRef, useState } from 'react';
+import React, { useRef } from 'react';
 import Button from '../components/ui/Button';
+import emailjs from '@emailjs/browser';
+import { useDispatch } from 'react-redux';
 
-export default function Contact () {
+export default function Contact() {
   const form = useRef();
   const dispatch = useDispatch();
 
   const closeEmail = () => {
-    dispatch({ type: "CLOSE_EMAIL" });
+    dispatch({ type: 'CLOSE_EMAIL' });
   };
 
   const sendEmail = (e) => {
     e.preventDefault();
     emailjs
       .sendForm(
-        "비밀",
-        "비밀",
+        'REACT_APP_EMAIL_SERVICE_ID',
+        'REACT_APP_EMAIL_TEMPLATE_ID',
         form.current,
-        "비밀"
+        'REACT_APP_EMAIL_PUBLIC_KEY'
       )
       .then(
         (result) => {
-          alert("전송되었습니다.");
+          alert('전송되었습니다.');
           closeEmail();
         },
         (error) => {
-          alert("전송을 실패했습니다.");
+          alert('전송을 실패했습니다.');
         }
       );
   };
 
   return (
-    <EmailForm ref={form} onSubmit={sendEmail}>
+    <form ref={form} onSubmit={sendEmail}>
       <Button onClick={closeEmail}> X </Button>
       <label>Name</label>
-      <input type="text" name="from_name" placeholder="이름을 입력해주세요." />
+      <input type='text' name='from_name' placeholder='이름을 입력해주세요.' />
       <label>Phone</label>
-      <input type="tel" name="phone" placeholder="연락처를 입력해주세요." />
+      <input type='tel' name='phone' placeholder='연락처를 입력해주세요.' />
       <label>Email</label>
-      <input type="email" name="email" placeholder="메일 주소를 입력해주세요" />
+      <input type='email' name='email' placeholder='메일 주소를 입력해주세요' />
       <label>Message</label>
-      <textarea name="text" />
-      <input type="submit" value="Send" />
-    </EmailForm>
+      <textarea name='text' />
+      <input type='submit' value='Send' />
+    </form>
   );
-  };
+}
